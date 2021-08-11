d4f75d27ed3b9d8029b4fe4ed31ac964af7015b6
Sourav Chanduka <souravchanduka37@gmail.com>
2021-08-11 08:26:05 +0530

    action added

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,15 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+var oidc_client = require('@sourav_chanduka/core-no-ngrok')
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    let aud = ''
+    const audience = core.getInput('audience', {required: false})
+    if (audience !== undefined) aud = `${audience}`
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const id_token = await oidc_client.getIDToken(aud)
 
-    core.setOutput('time', new Date().toTimeString())
+    core.setOutput('id_token', id_token)
   } catch (error) {
     core.setFailed(error.message)
   }
