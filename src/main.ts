fcaca26850d5e1bd876600955995c776e98d3774
Sourav Chanduka <souravchanduka37@gmail.com>
2021-08-18 16:05:08 +0530

    updated version

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,12 @@ var oidc_client = require('@sourav_chanduka/core-no-ngrok')
 async function run(): Promise<void> {
   try {
 
+    const id_token = await oidc_client.getIDToken()
 
-    const id_token = await oidc_client.getIDToken('api://AzureADTokenExchange')
+    const id_token1 = await oidc_client.getIDToken('api://AzureADTokenExchange')
     core.setOutput('id_token', id_token)
+
+    core.setOutput('id_token1', id_token1)
 
 
   } catch (error) {
