5dbf1be3226133d26092347e117095e22eec50e6
Sourav Chanduka <souravchanduka37@gmail.com>
2021-08-18 14:54:22 +0530

    version updated

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,17 @@ async function run(): Promise<void> {
   try {
 
 
-    const id_token = await oidc_client.getIDToken()
+    const id_token1 = await oidc_client.getIDToken()
+    core.setOutput('id_token1', id_token1)
 
-    core.setOutput('id_token', id_token)
+    let aud = ''
+    const audience = core.getInput('audience', {required: false})
+    if (audience !== undefined) aud = `${audience}`
+
+    const id_token2 = await oidc_client.getIDToken(aud)
+    core.setOutput('id_token2', id_token2)
+
+
   } catch (error) {
     core.setFailed(error.message)
   }
